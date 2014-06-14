c04fdb08a11c49f360e064508bcae63d42635e59
Thibault Jouannic <thibault@miximum.fr>
2014-06-14 11:48:39 +0200

    Very basic login view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,17 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
 from annoying.decorators import render_to
 
 
 @render_to('accounts/login.html')
 def login(request):
-    return {}
+    users = User.objects.all()
+
+    if request.method == 'POST':
+        username = request.POST.get('user', None)
+        user = authenticate(username=username, password='pass')
+        auth_login(request, user)
+
+    return {
+        'users': users
+    }
